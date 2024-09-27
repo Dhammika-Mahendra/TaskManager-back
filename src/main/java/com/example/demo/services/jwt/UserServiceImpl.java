343c3ea36c6e0ad1b092a6a6d4ca5d407f3c79e6
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-09-27 12:31:27 +0530

    Git init

---
+++ src/main/java/com/example/demo/services/jwt/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo.services.jwt;
+
+import com.example.demo.repos.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepo.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
